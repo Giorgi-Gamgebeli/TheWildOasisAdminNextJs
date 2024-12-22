9f961dc110262744384491e039b04b7a94db92e7
CodingReaper1 <giorgi.gamertube@gmail.com>
2024-12-22 12:49:52 +0400

    experiment failed

--- app/_components/NavLink.tsx
+++ app/_components/NavLink.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-// import Link from "next/link";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 type NavLinkProps = {
@@ -13,15 +13,10 @@ function NavLink({ children, href, text }: NavLinkProps) {
   const pathname = usePathname();
   const isActive = pathname === href;
 
-  function handleClick() {
-    history.pushState(null, "", pathname);
-  }
-
   return (
-    <button
+    <Link
       className={`hover flex items-center gap-[1.2rem] px-[2.4rem] py-[1.2rem] text-[2.4rem] transition-all duration-300 ${isActive ? "rounded-md bg-gray-50 text-indigo-600 dark:bg-gray-900 dark:text-indigo-600" : "text-gray-400 hover:rounded-md hover:bg-gray-50 hover:text-indigo-600 dark:text-gray-500 hover:dark:bg-gray-900 hover:dark:text-indigo-600"}`}
-      // href={href}
-      onClick={handleClick}
+      href={href}
     >
       {children}
       <span
@@ -29,7 +24,7 @@ function NavLink({ children, href, text }: NavLinkProps) {
       >
         {text}
       </span>
-    </button>
+    </Link>
   );
 }
 
